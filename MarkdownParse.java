1f74a8cdfe16bb9f68196df3ddd732b021a441ed
minjismin <mik019@ucsd.edu>
2022-04-24 19:10:38 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import static org.junit.Assert.*;
-import org.junit.*;
+// import static org.junit.Assert.*;
+// import org.junit.*;
 
 public class MarkdownParse {
 
